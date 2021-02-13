9990e4e16cd613cf58300a3f87827a4338700a20
lealemgetahun <lealemgetahun73@gmail.com>
2021-02-13 02:14:25 -0800

    initial commt for flutter

---
+++ online_customer_care/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ 
+  final CommentRepository commentRepository = CommentRepository(dataProvider: CommentDataProvider());
+  runApp(MyApp(
+    
+  ));
+}
+
+}
+
